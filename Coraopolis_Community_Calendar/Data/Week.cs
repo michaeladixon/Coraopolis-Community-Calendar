dbedc8cc3f5bf7be11d38ee24667536f3354415b
micha <micha@ALIENWARER8>
2023-02-05 07:17:56 -0500

    Add project files.

---
+++ Coraopolis_Community_Calendar/Data/Week.cs
@@ -0,0 +1,8 @@
+﻿namespace Coraopolis_Community_Calendar.Data
+{
+    public class Week
+    {
+        public ICollection<Day> Days { get; set; } = new List<Day>();
+
+    }
+}
---
+++ Coraopolis_Community_Calendar/Data/Year.cs
@@ -0,0 +1,7 @@
+﻿namespace Coraopolis_Community_Calendar.Data
+{
+    public class Year
+    {
+        public ICollection<Month> Months { get; set; } = new List<Month>();
+    }
+}
